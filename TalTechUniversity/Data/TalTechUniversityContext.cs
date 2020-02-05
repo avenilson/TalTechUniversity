3b10e6abe300860df3e5782bf8e89a0d56062b88
Ave Nilson <avnils@ttu.ee>
2020-02-05 18:46:33 +0200

    Lesson 2-5

--- TalTechUniversity/Data/TalTechUniversityContext.cs
+++ TalTechUniversity/Data/TalTechUniversityContext.cs
@@ -9,8 +9,7 @@ namespace TalTechUniversity.Data
 {
     public class TalTechUniversityContext : DbContext
     {
-        public TalTechUniversityContext (DbContextOptions<TalTechUniversityContext> options)
-            : base(options)
+        public TalTechUniversityContext (DbContextOptions<TalTechUniversityContext> options): base(options)
         {
         }
 
@@ -19,12 +18,22 @@ namespace TalTechUniversity.Data
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
             modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            modelBuilder.Entity<CourseAssignment>().HasKey(c => new { c.CourseID, c.InstructorID });
         }
     }
 }
---
+++ TalTechUniversity/Pages/About.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TalTechUniversity.Data;
+using TalTechUniversity.Models.SchoolViewModels;
+
+namespace TalTechUniversity.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly TalTechUniversityContext _context;
+
+        public AboutModel(TalTechUniversityContext context)
+        {
+            _context = context;
+        }
+
+        public IList<EnrollmentDateGroup> Students { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+
+            Students = await data.AsNoTracking().ToListAsync();
+        }
+    }
+}
